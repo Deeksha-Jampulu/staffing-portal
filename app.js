5edbe00c571ca307c5d688627fbb862fcf91d1d8
Deeksha-Jampulu <deekshajampulu@gmail.com>
2025-12-18 15:35:56 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// Paste your Apps Script Web App URL (ends with /exec)
+const API_BASE = "https://script.google.com/macros/s/AKfycby2DvyIRUqqbpKmg8JM5sQJXO37TqWSI1Qbx4fiOJu8uJwzpCENkWW2smZw61kkdSXpeA/exec";
+
+async function apiGet(route, params = {}) {
+  const url = new URL(API_BASE);
+  url.searchParams.set("route", route);
+  for (const [k, v] of Object.entries(params)) url.searchParams.set(k, v);
+  const res = await fetch(url.toString());
+  return res.json();
+}
+
+async function apiPost(payload) {
+  const res = await fetch(API_BASE, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+  return res.json();
+}
+
+function esc(s) {
+  return String(s ?? "").replace(/[&<>"']/g, m => ({
+    "&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#039;"
+  }[m]));
+}
+
+function qs(name) {
+  return new URLSearchParams(window.location.search).get(name);
+}
