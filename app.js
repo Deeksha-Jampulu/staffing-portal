3e427e69246322d3e0d986c73bbecc4ec494d3e8
Deeksha-Jampulu <deekshajampulu@gmail.com>
2025-12-19 11:20:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
-// Paste your Apps Script Web App URL (ends with /exec)
 const API_BASE = "https://script.google.com/macros/s/AKfycby2DvyIRUqqbpKmg8JM5sQJXO37TqWSI1Qbx4fiOJu8uJwzpCENkWW2smZw61kkdSXpeA/exec";
 
-async function apiGet(route, params = {}) {
-  const url = new URL(API_BASE);
-  url.searchParams.set("route", route);
-  for (const [k, v] of Object.entries(params)) url.searchParams.set(k, v);
-  const res = await fetch(url.toString());
-  return res.json();
-}
+function jsonp(route, params = {}) {
+  return new Promise((resolve, reject) => {
+    const callbackName = "cb_" + Date.now();
 
-async function apiPost(payload) {
-  const res = await fetch(API_BASE, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload),
+    window[callbackName] = function (data) {
+      resolve(data);
+      delete window[callbackName];
+      script.remove();
+    };
+
+    const url = new URL(API_BASE);
+    url.searchParams.set("route", route);
+    url.searchParams.set("callback", callbackName);
+
+    Object.keys(params).forEach(key => {
+      if (params[key]) url.searchParams.set(key, params[key]);
+    });
+
+    const script = document.createElement("script");
+    script.src = url.toString();
+
+    script.onerror = function () {
+      reject(new Error("API load failed"));
+      delete window[callbackName];
+      script.remove();
+    };
+
+    document.body.appendChild(script);
   });
-  return res.json();
 }
 
 function esc(s) {
-  return String(s ?? "").replace(/[&<>"']/g, m => ({
-    "&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#039;"
-  }[m]));
+  return String(s || "")
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
 }
-
-function qs(name) {
-  return new URLSearchParams(window.location.search).get(name);
-}
-
-
