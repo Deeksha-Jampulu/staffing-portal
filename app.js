1ba1b2702845b449605f54c4df4d8e0da83a1db3
Deeksha-Jampulu <deekshajampulu@gmail.com>
2025-12-22 10:05:47 -0600

    Fix API URL

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const API_URL = "https://script.google.com/macros/s/AKfycby2DvyIRUqqbpKmg8JM5sQJ
 
 async function apiGet(route) {
   const res = await fetch(`${API_URL}?route=${route}`);
-  return res.json();
+  return await res.json();
 }
 
 async function apiPost(route, payload) {
   const res = await fetch(`${API_URL}?route=${route}`, {
     method: "POST",
-    body: JSON.stringify(payload)
+    body: JSON.stringify(payload),
   });
-  return res.json();
+  return await res.json();
 }
